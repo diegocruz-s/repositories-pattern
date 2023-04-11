eaae6a3908f183af20f37fa30dd12318db87acd4
Diego Cruz <diegoszcruz1001@gmail.com>
2023-04-11 20:55:54 -0300

    feat: add GetUsersController

---
+++ src/controllers/get-users/get-users.ts
@@ -0,0 +1,33 @@
+import { User } from "../../models/user";
+import { IGetUsersController, IGetUsersRepository } from "./protocols";
+
+export class GetUsersControllers implements IGetUsersController {
+    getUsersRepository: IGetUsersRepository
+    
+    constructor(getUsersRepository: IGetUsersRepository){
+        this.getUsersRepository = getUsersRepository
+    }
+
+    // posso resumir tudo isso para isso:
+    //constructor(private readonly getUsersRepository: IGetUsersRepository) {}
+    
+    async handle() {
+        // validar requisição
+        //direcionar chamada ao repository
+
+        try {
+            const users = await this.getUsersRepository.getUsers()
+            return {
+                statusCode: 200,
+                body: users 
+            }
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: 'Something went wrong'
+            }
+        }
+
+
+    }
+}
