e36c60eaeff53504e3e8524d5ee8d555cde49301
Diego Cruz <diegoszcruz1001@gmail.com>
2023-04-12 19:33:18 -0300

    refactor: add HttpStatusCode enum

--- src/controllers/protocols.ts
+++ src/controllers/protocols.ts
@@ -1,5 +1,5 @@
 export interface HttpResponse<T> {
-    statusCode: number
+    statusCode: HttpStatusCode
     body: T 
 }
 
@@ -12,4 +12,11 @@ export interface HttpRequest<B> {
 export interface IController {
     handle(httpResquest: HttpRequest<unknown>): Promise<HttpResponse<unknown>>
         // unknown o tipo vai ser definido quando eu de fato usar o método, no momento eu não sei
+}
+
+export enum HttpStatusCode {
+    OK = 200,
+    CREATED = 201,
+    BAD_REQUEST = 400,
+    SERVER_ERROR = 500,
 }
