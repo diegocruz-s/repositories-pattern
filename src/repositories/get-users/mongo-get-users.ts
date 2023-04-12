e872a339d96e190642e7f74307cfc5932c576b60
Diego Cruz <diegoszcruz1001@gmail.com>
2023-04-11 22:49:07 -0300

    feat: add id to user

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,9 +1,13 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepositories implements IGetUsersRepository {
     async getUsers(): Promise<User[]> {
+        const users = await MongoClient.db.collection<User>('users').find({}).toArray()
+
         return [{
+            id: '',
             firstName: 'Diego M.',
             lastName: 'Cruz',
             email: 'diego@gmail.com',
@@ -15,6 +19,7 @@ export class MongoGetUsersRepositories implements IGetUsersRepository {
 export class PostgresGetUsersRepositories implements IGetUsersRepository {
     async getUsers(): Promise<User[]> {
         return [{
+            id: '',
             firstName: 'Diego P.',
             lastName: 'Cruz',
             email: 'diego@gmail.com',
